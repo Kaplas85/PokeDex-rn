4178cac3c81250c42667c338744567f9237907ae
Cesar Baudi <cesarbaudi1@gmail.com>
2021-12-23 09:55:56 -0400

    fix: removed console.log with pokemons array

--- src/screen/PokedexScreen.jsx
+++ src/screen/PokedexScreen.jsx
@@ -8,7 +8,6 @@ export default function PokedexScreen() {
   const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  console.log(pokemons);
 
   useEffect(() => {
     (async () => {
